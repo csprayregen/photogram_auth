4a3a53f6a0f67702b87622731d7e11e234ace332
csprayregen <csprayregen@users.noreply.github.com>
2017-05-31 14:52:02 -0500

    thru first validation in the read.me

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+
+  belongs_to :photo, :class_name => "Photo", :foreign_key => "photo_id"
+
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+
+end
---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+class Like < ApplicationRecord
+
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+
+  belongs_to :photo, :class_name => "Photo", :foreign_key => "photo_id"
+
+end
