73d831f386ba09fdba9b9b32cb1a9ccb1014b240
csprayregen <csprayregen@users.noreply.github.com>
2017-06-03 19:05:27 -0500

    started devise updates

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,6 +12,7 @@ class LikesController < ApplicationController
   end
 
   def my_likes
+    @likes = current_user.liked_photos
     render("likes/my_likes.html.erb")
   end
 
@@ -30,9 +31,9 @@ class LikesController < ApplicationController
     save_status = @like.save
 
     if save_status == true
-      redirect_to("/likes/#{@like.id}", :notice => "Like created successfully.")
+      redirect_to("/photos", :notice => "Like created successfully.")
     else
-      render("likes/new.html.erb")
+      redirect_to("/photos")
     end
   end
 
